eb42067cd9f31e7a2156b8c8670811b23bea7119
Dayana <Ddiaz413@west-mec.oeg>
2024-08-15 13:14:51 -0700

    updated files

--- finalcode.js
+++ finalcode.js
@@ -2,4 +2,25 @@ const prompt = require("node-sync")();
 
 const gravityFactors = require('./utils/earthGravityFactors.js');
 
-function showUserFactors() {}
+function showUserFactors(type, value) {
+
+    let results = [];
+    let measurement;
+
+    for(let planet in gravityFactors) {
+        results[planet] = ((gravityFactors[planet] * value).toFixed(2));
+
+    }
+
+    switch[type] {
+        case "jump";
+        measurement = "cm";
+        break;
+        case "liquid";
+        measurement = "vol";
+        break;
+        default:
+        measurement = "na";
+    }
+
+}
